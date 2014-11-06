6a75474c2f4dadb397d2059e56492601bc429c13
Brandon Francis <brandon@brandon-francis.com>
2014-11-06 12:29:09 -0500

    Removed var dumps

--- app/models/question.php
+++ app/models/question.php
@@ -632,8 +632,6 @@ class QuestionAnswer {
      */
     public function hasLiked(User $user) {
         $users = $this->getLikes();
-        var_dump($users);
-        var_dump($user);
         for ($i = 0; $i < count($users); $i++) {
             if ($users[$i]->getUserId() === $user->getUserId()) {
                 return true;
