9f90501a3a236e8cd63595b30c5339ae2e45184f
Brandon Francis <brandon@brandon-francis.com>
2014-12-11 14:15:02 -0500

    Updated install script

--- install/index.php
+++ install/index.php
@@ -6,14 +6,22 @@ require_once '../core/autoload.php';
 
 // Check to see if a connection to the database could be established
 try {
-    
+
     // Get the connection
     $connection = Database::connection();
-    
 } catch (Exception $ex) {
 
-    
+    View::renderView('install_splash', array('show_button' => fasle, 'result' => 'Database connection was not successful.'));
     
 }
 
-View::renderView('install');
+// See if we're doing the installation
+if (Input::exixts('do_install')) {
+
+    // We're doing the installation
+    View::renderView('install_result', array('result' => 'Nothing was done.'));
+    
+}
+
+
+View::renderView('install_splash', array('show_button' => true, 'result' => 'Database connection was successful.'));
